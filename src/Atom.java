e4d5a74686cc277d021b584f478dfde166aabf52
Estef072 <61289015+Estef072@users.noreply.github.com>
2021-04-07 15:31:32 -0600

    Add files via upload

---
+++ src/Atom.java
@@ -0,0 +1,29 @@
+
+import java.util.ArrayList;
+
+public class Atom{
+
+
+    public String traducirA (Arrarlist<String> index){
+        ArrayList <String> dato = new Arraylist <String> (); 
+        for (String palabra : index){
+            if(palabra.equals("atom")){
+
+            }
+            else {
+                dato.add(palabra);
+
+            }
+        }
+        if (dato.size()==1){
+            return "True";
+
+        }
+        else {
+            return "False";
+
+        }
+
+
+    }
+}
